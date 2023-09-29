6c3b3da40bb9e94de07c95e113abb15b22d54e36
Legrix Jeremy <jeremy.legrix@hotmail.com>
2023-09-29 11:38:42 +0200

    Ajout de la grille dynamique

--- Hitori/MainPage.xaml.cs
+++ Hitori/MainPage.xaml.cs
@@ -22,9 +22,81 @@ namespace Hitori
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private int[,] hitoriMatrix;
+        private int gridLenght = 5;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.CreateHitoriMatrix();
+
+            this.CreateDynamicGrid();
+        }
+
+        private void CreateDynamicGrid()
+        {
+            Grid dynamicGrid = new Grid();
+
+            for (int i = 0; i < this.gridLenght; i++)
+            {
+                RowDefinition rowDefinition = new RowDefinition();
+                rowDefinition.Height = GridLength.Auto; // Vous pouvez ajuster la hauteur selon vos besoins
+                dynamicGrid.RowDefinitions.Add(rowDefinition);
+
+                ColumnDefinition columnDefinition = new ColumnDefinition();
+                columnDefinition.Width = GridLength.Auto; // Vous pouvez ajuster la largeur selon vos besoins
+                dynamicGrid.ColumnDefinitions.Add(columnDefinition);
+            }
+
+            for (int row = 0; row < this.gridLenght; row++)
+            {
+                for (int col = 0; col < this.gridLenght; col++)
+                {
+                    Button button = new Button();
+                    button.Content = $"{this.hitoriMatrix[row, col]}"; // Texte de chaque bouton
+                    Grid.SetRow(button, row);
+                    Grid.SetColumn(button, col);
+                    dynamicGrid.Children.Add(button);
+                }
+            }
+
+            DynamicGrid.Children.Add(dynamicGrid);
+        }
+
+        private void CreateHitoriMatrix()
+        {
+            this.hitoriMatrix = new int[this.gridLenght, this.gridLenght];
+
+            this.hitoriMatrix[0, 0] = 4;
+            this.hitoriMatrix[0, 1] = 2;
+            this.hitoriMatrix[0, 2] = 1;
+            this.hitoriMatrix[0, 3] = 5;
+            this.hitoriMatrix[0, 4] = 3;
+
+            this.hitoriMatrix[1, 0] = 1;
+            this.hitoriMatrix[1, 1] = 4;
+            this.hitoriMatrix[1, 2] = 3;
+            this.hitoriMatrix[1, 3] = 2;
+            this.hitoriMatrix[1, 4] = 2;
+
+            this.hitoriMatrix[2, 0] = 3;
+            this.hitoriMatrix[2, 1] = 5;
+            this.hitoriMatrix[2, 2] = 4;
+            this.hitoriMatrix[2, 3] = 2;
+            this.hitoriMatrix[2, 4] = 2;
+
+            this.hitoriMatrix[3, 0] = 1;
+            this.hitoriMatrix[3, 1] = 5;
+            this.hitoriMatrix[3, 2] = 2;
+            this.hitoriMatrix[3, 3] = 5;
+            this.hitoriMatrix[3, 4] = 5;
+
+            this.hitoriMatrix[4, 0] = 1;
+            this.hitoriMatrix[4, 1] = 1;
+            this.hitoriMatrix[4, 2] = 5;
+            this.hitoriMatrix[4, 3] = 3;
+            this.hitoriMatrix[4, 4] = 4;
         }
     }
 }
