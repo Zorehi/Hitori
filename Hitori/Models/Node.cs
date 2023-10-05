678c01d391cad74e629b23fe0d9abe30466de81b
CyrilJack <jacques.cyril80@gmail.com>
2023-10-05 10:42:35 +0200

    Classe Node

---
+++ Hitori/Models/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hitori.Models
+{
+    class Node
+    {
+        private int Id;
+        private int[] AdjaList;
+
+        public Node(int pos)
+        {
+
+        }
+
+    }
+}
