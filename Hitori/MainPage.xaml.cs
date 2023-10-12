9325d946e46b47de5ac220d5dbd423917d65b945
Zorehi <jeremy.legrix@hotmail.com>
2023-10-12 23:55:55 +0200

    Ajout du bouton check et du responsive de la page

--- Hitori/MainPage.xaml.cs
+++ Hitori/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -19,89 +20,108 @@ using Windows.UI.Xaml.Shapes;
 
 namespace Hitori
 {
-    /// <summary>
-    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        private Box[,] hitoriMatrix;
-        private int gridLenght = 9;
+	/// <summary>
+	/// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		private Box[,] hitoriMatrix;
+		private int gridLenght = 9;
 
-        public MainPage()
-        {
-            this.InitializeComponent();
-            this.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 30, 30 ,30));
+		public MainPage()
+		{
+			this.InitializeComponent();
 
-            this.hitoriMatrix = Hitori.Models.Matrix.GenerateMatrix(this.gridLenght);
+			this.Background = new SolidColorBrush(Color.FromArgb(255, 30, 30 ,30));
 
-            this.CreateDynamicGrid();
-        }
+			this.hitoriMatrix = Models.Matrix.GenerateMatrix(this.gridLenght);
 
-        private void CreateDynamicGrid()
-        {
-            Grid dynamicGrid = new Grid();
+			this.CreateDynamicGrid();
+		}
 
-            for (int i = 0; i < this.gridLenght; i++)
-            {
-                RowDefinition rowDefinition = new RowDefinition();
-                rowDefinition.Height = GridLength.Auto; // Vous pouvez ajuster la hauteur selon vos besoins
-                dynamicGrid.RowDefinitions.Add(rowDefinition);
+		private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			foreach (var item in DynamicGrid.ColumnDefinitions)
+			{
+				item.MinWidth = (Window.Current.Bounds.Height * 0.85) / this.gridLenght;
+			}
+			foreach (var item in DynamicGrid.RowDefinitions)
+			{
+				item.MinHeight = (Window.Current.Bounds.Height * 0.85) / this.gridLenght;
+			}
+		
+			for (int row = 0; row < this.gridLenght; row++)
+			{
+				for (int col = 0; col < this.gridLenght; col++)
+				{
+					this.hitoriMatrix[row, col].Button.FontSize = this.hitoriMatrix[row, col].Button.ActualHeight / 3;
+				}
+			}
+		}
 
-                ColumnDefinition columnDefinition = new ColumnDefinition();
-                columnDefinition.Width = GridLength.Auto; // Vous pouvez ajuster la largeur selon vos besoins
-                dynamicGrid.ColumnDefinitions.Add(columnDefinition);
-            }
+		private void CreateDynamicGrid()
+		{
+			for (int i = 0; i < this.gridLenght; i++)
+			{
+				RowDefinition rowDefinition = new RowDefinition();
+				rowDefinition.MinHeight = (Window.Current.Bounds.Height * 0.85) / this.gridLenght;
+				DynamicGrid.RowDefinitions.Add(rowDefinition);
 
-            for (int row = 0; row < this.gridLenght; row++)
-            {
-                for (int col = 0; col < this.gridLenght; col++)
-                {
-                    Button button = new Button();
-                    button.Content = $"{this.hitoriMatrix[row, col].Value}"; // Texte de chaque bouton
+				ColumnDefinition columnDefinition = new ColumnDefinition();
+				columnDefinition.MinWidth = (Window.Current.Bounds.Height * 0.85) / this.gridLenght;
+				DynamicGrid.ColumnDefinitions.Add(columnDefinition);
+			}
 
-                    button.Click += ChangeColorButton_Click;
-                    button.DataContext = this.hitoriMatrix[row, col];
-                    button.Background = new SolidColorBrush(Windows.UI.Colors.Black);
-                    button.Height = (Window.Current.Bounds.Height * 0.85) / this.gridLenght;
-                    button.Width = button.Height;
-                    button.FontSize = button.Height / 3;
-                    button.Margin = new Thickness(1);
+			for (int row = 0; row < this.gridLenght; row++)
+			{
+				for (int col = 0; col < this.gridLenght; col++)
+				{
+					Button button = new Button();
+					button.Content = $"{this.hitoriMatrix[row, col].Value}"; // Texte de chaque bouton
 
-                    this.hitoriMatrix[row, col].Button = button;
-                    Grid.SetRow(button, row);
-                    Grid.SetColumn(button, col);
+					button.Style = ButtonWithoutHover;
 
-                    dynamicGrid.Children.Add(button);
-                }
-            }
+					button.Click += ChangeColorButton_Click;
+					button.DataContext = this.hitoriMatrix[row, col];
+					button.Background = new SolidColorBrush(Colors.Black);
+					//button.Height = (Window.Current.Bounds.Height * 0.85) / this.gridLenght;.
+					button.VerticalAlignment = VerticalAlignment.Stretch;
+					button.HorizontalAlignment = HorizontalAlignment.Stretch;
+					//button.Width = button.Height;
+					//button.FontSize = button.ActualHeight / 3;
+					button.Margin = new Thickness(1);
 
-            DynamicGrid.Children.Add(dynamicGrid);
-        }
+					this.hitoriMatrix[row, col].Button = button;
+					Grid.SetRow(button, row);
+					Grid.SetColumn(button, col);
 
-        private void ChangeColorButton_Click(object sender, RoutedEventArgs e)
-        {
-            Button button = sender as Button;
-            Box box = button.DataContext as Box;
+					DynamicGrid.Children.Add(button);
+				}
+			}
+		}
 
-            Console.WriteLine(box.State);
+		private void ChangeColorButton_Click(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			Box box = button.DataContext as Box;
 
-            switch (box.State)
-            {
-                case State.Black:
-                    button.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 90, 90, 90));
-                    box.State = State.Gray;
-                    break;
-                case State.White:
-                    button.Background = new SolidColorBrush(Windows.UI.Colors.Black);
-                    button.Foreground = new SolidColorBrush(Windows.UI.Colors.White);
-                    box.State = State.Black;
-                    break;
-                case State.Gray:
-                    button.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 170, 170, 170));
-                    button.Foreground = new SolidColorBrush(Windows.UI.Colors.Black);
-                    box.State = State.White;
-                    break;
-            }
-        }
-    }
+			switch (box.State)
+			{
+				case State.Black:
+					button.Background = new SolidColorBrush(Color.FromArgb(255, 90, 90, 90));
+					box.State = State.Gray;
+					break;
+				case State.White:
+					button.Background = new SolidColorBrush(Colors.Black);
+					button.Foreground = new SolidColorBrush(Colors.White);
+					box.State = State.Black;
+					break;
+				case State.Gray:
+					button.Background = new SolidColorBrush(Color.FromArgb(255, 200, 200, 200));
+					button.Foreground = new SolidColorBrush(Colors.Black);
+					box.State = State.White;
+					break;
+			}
+		}
+	}
 }
--- Hitori/Models/Matrix.cs
+++ Hitori/Models/Matrix.cs
@@ -12,119 +12,28 @@ namespace Hitori.Models
     {
         public static Box[,] GenerateMatrix(int length)
         {
-            switch (length)
-            {
-                case 5:
-                    return Matrix.GenerateMatrix5x5();
-                case 7:
-                    return Matrix.GenerateMatrix7x7();
-                case 9:
-                    return Matrix.GenerateMatrix9x9();
-                default:
-                    throw new Exception("Impossiblle de générer une matrice "+ length + "x" + length);
-            }
-        }
+			if (length != 5 && length != 7 && length != 9)
+			{
+				throw new Exception($"Impossible de généré une matrice {length}x{length}");
+			}
 
-        private static Box[,] GenerateMatrix5x5()
-        {
-            string filename = "Assets/Matrix/Matrix5x5.txt";
-            int length = 5;
-            Box[,] matrix = new Box[length, length];
+			string filename = $"Assets/Matrix/Matrix{length}x{length}.txt";
+			Box[,] matrix = new Box[length, length];
 
-            using (var reader = new StreamReader(filename))
-            {
-                for (int i = 0; i < length; i++)
-                {
-                    string line = reader.ReadLine();
-                    string[] separateLine = line.Split(' ');
-                    for (int j = 0; j < length; j++)
-                    {
-                        matrix[i, j] = new Box(int.Parse(separateLine[j]), State.Black);
-                    }
-                }
-            }
+			using (var reader = new StreamReader(filename))
+			{
+				for (int i = 0; i < length; i++)
+				{
+					string line = reader.ReadLine();
+					string[] separateLine = line.Split(' ');
+					for (int j = 0; j < length; j++)
+					{
+						matrix[i, j] = new Box(int.Parse(separateLine[j]), State.Black);
+					}
+				}
+			}
 
-            return matrix;
-        }
-
-        private static Box[,] GenerateMatrix7x7()
-        {
-            string filename = "Assets/Matrix/Matrix7x7.txt";
-            int length = 7;
-            Box[,] matrix = new Box[length, length];
-
-            using (var reader = new StreamReader(filename))
-            {
-                for (int i = 0; i < length; i++)
-                {
-                    string line = reader.ReadLine();
-                    string[] separateLine = line.Split(' ');
-                    for (int j = 0; j < length; j++)
-                    {
-                        matrix[i, j] = new Box(int.Parse(separateLine[j]), State.Black);
-                    }
-                }
-            }
-
-            return matrix;
-        }
-
-        private static Box[,] GenerateMatrix9x9()
-        {
-            string filename = "Assets/Matrix/Matrix9x9.txt";
-            int length = 9;
-            Box[,] matrix = new Box[length, length];
-
-            using (var reader = new StreamReader(filename))
-            {
-                for (int i = 0; i < length; i++)
-                {
-                    string line = reader.ReadLine();
-                    string[] separateLine = line.Split(' ');
-                    for (int j = 0; j < length; j++)
-                    {
-                        matrix[i, j] = new Box(int.Parse(separateLine[j]), State.Black);
-                    }
-                }
-            }
-
-            return matrix;
-        }
-        /*private static Box[,] GenerateMatrix5x5()
-        {
-            Box[,] matrix = new Box[5, 5];
-
-            matrix[0, 0] = new Box(4, State.Black);
-            matrix[0, 1] = new Box(2, State.Black);
-            matrix[0, 2] = new Box(1, State.Black);
-            matrix[0, 3] = new Box(5, State.Black);
-            matrix[0, 4] = new Box(3, State.Black);
-
-            matrix[1, 0] = new Box(1, State.Black);
-            matrix[1, 1] = new Box(4, State.Black);
-            matrix[1, 2] = new Box(3, State.Black);
-            matrix[1, 3] = new Box(2, State.Black);
-            matrix[1, 4] = new Box(2, State.Black);
-
-            matrix[2, 0] = new Box(3, State.Black);
-            matrix[2, 1] = new Box(5, State.Black);
-            matrix[2, 2] = new Box(4, State.Black);
-            matrix[2, 3] = new Box(2, State.Black);
-            matrix[2, 4] = new Box(2, State.Black);
-
-            matrix[3, 0] = new Box(1, State.Black);
-            matrix[3, 1] = new Box(5, State.Black);
-            matrix[3, 2] = new Box(2, State.Black);
-            matrix[3, 3] = new Box(5, State.Black);
-            matrix[3, 4] = new Box(5, State.Black);
-
-            matrix[4, 0] = new Box(1, State.Black);
-            matrix[4, 1] = new Box(1, State.Black);
-            matrix[4, 2] = new Box(5, State.Black);
-            matrix[4, 3] = new Box(3, State.Black);
-            matrix[4, 4] = new Box(4, State.Black);
-
-            return matrix;
-        }*/
+			return matrix;
+		}
     }
 }
