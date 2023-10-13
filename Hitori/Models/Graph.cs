c578f23e8889a61a80b0dbcbff7bba3c2d66ba26
CyrilJack <jacques.cyril80@gmail.com>
2023-10-13 10:51:41 +0200

    Nouvelles classes

---
+++ Hitori/Models/Graph.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hitori.Models
+{
+    class Graph
+    {
+        private Node[,] graph;
+
+        public Graph(Box[,] matrix)
+        {
+            int x= matrix.Length;
+            int y = x; 
+
+            for(int i =0; i<x; i++)
+            {
+                for(int j=0; j < y; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
--- Hitori/Models/Node.cs
+++ Hitori/Models/Node.cs
@@ -8,12 +8,17 @@ namespace Hitori.Models
 {
     class Node
     {
-        private int Id;
-        private int[] AdjaList;
+        private int xpos,ypos;
+        private List<Node> AdjaList;
 
-        public Node(int pos)
+        public Node(int x, int y,  List<Node> list)
         {
-
+            xpos = x;
+            ypos = y;
+            for (int i = 0; i <= list.Count; i++)
+            {
+                AdjaList.Add(list[i]);
+            }
         }
 
     }
