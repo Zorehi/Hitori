5811c734eaae4119462c52ffd64b6e00d4cca891
CyrilJack <jacques.cyril80@gmail.com>
2023-10-13 13:46:37 +0200

    Classes Graph

--- Hitori/Models/Graph.cs
+++ Hitori/Models/Graph.cs
@@ -8,20 +8,108 @@ namespace Hitori.Models
 {
     class Graph
     {
-        private Node[,] graph;
+        private Node[,] _nodes;
 
-        public Graph(Box[,] matrix)
+        public Graph(int[,] matrix)
         {
-            int x= matrix.Length;
-            int y = x; 
-
+            int x = matrix.GetLength(0);
+            List<Node> list = new List<Node>();
             for(int i =0; i<x; i++)
             {
-                for(int j=0; j < y; j++)
+                for(int j=0; j < x; j++)
                 {
 
+                    Node node = new Node(i, j, matrix[i, j], State.Black);
+                    _nodes[i, j] = node;
+                }
+            }
+            for(int k=0; k<x; k++)
+            {
+                for(int l=0; l<x; k++)
+                {
+                    // Coin haut-gauche
+                    if ( k == 0 && l == 0)
+                    {
+                        list.Add(_nodes[k + 1,l]);
+                        list.Add(_nodes[k, l + 1]);
+                        _nodes[k, l].AdjaList = list;
+                        list.Clear();
+                    }
+                    // Coin haut-droit
+                    if (k == 0 && l == x - 1)
+                    {
+                        list.Add(_nodes[k, l - 1]);
+                        list.Add(_nodes[k + 1, l]);
+                        _nodes[k, l].AdjaList = list;
+                        list.Clear();
+                    }
+                    // Coin bas-gauche
+                    if (k == x-1 && l == 0)
+                    {
+                        list.Add(_nodes[k, l + 1]);
+                        list.Add(_nodes[k-1, l]);
+                        _nodes[k, l].AdjaList = list;
+                        list.Clear();
+                    }
+                    // Coin bas-groit
+                    if (k == x-1 && l == x-1)
+                    {
+                        list.Add(_nodes[k - 1, l]);
+                        list.Add(_nodes[k, l - 1]);
+                        _nodes[k, l].AdjaList = list;
+                        list.Clear();
+                    }
+                    // Ligne du haut
+                    if ( k == 0)
+                    {
+                        list.Add(_nodes[k + 1, l]);
+                        list.Add(_nodes[k, l-1]);
+                        list.Add(_nodes[k, l+1]);
+                        _nodes[0, l].AdjaList = list;
+                        list.Clear();
+                    
+                    }
+                    // Ligne du bas
+                    if (k == x - 1)
+                    {
+                        list.Add(_nodes[k - 1, l]);
+                        list.Add(_nodes[k, l - 1]);
+                        list.Add(_nodes[k, l + 1]);
+                        _nodes[k, l].AdjaList = list;
+                        list.Clear();
+                    }
+                    //Colonne de gauche
+                    if (l == 0)
+                    {
+                        list.Add(_nodes[k - 1, l]);
+                        list.Add(_nodes[k + 1, l]);
+                        list.Add(_nodes[k, l + 1]);
+                        _nodes[k, l].AdjaList = list;
+                        list.Clear();
+                    }
+                    // Colonne de droite
+                    if (l == x - 1)
+                    {
+                        list.Add(_nodes[k - 1, l]);
+                        list.Add(_nodes[k + 1, l]);
+                        list.Add(_nodes[k, l - 1]);
+                        _nodes[k, l].AdjaList = list;
+                        list.Clear();
+                    }
+                    else
+                    {
+                        list.Add(_nodes[k - 1, l]);
+                        list.Add(_nodes[k + 1, l]);
+                        list.Add(_nodes[k, l - 1]);
+                        list.Add(_nodes[k, l + 1]);
+                        _nodes[k, l].AdjaList = list;
+                        list.Clear();
+                    }
                 }
             }
         }
+
+        public Node[,] Nodes { get => _nodes; set => _nodes = value; }
+
     }
 }
--- Hitori/Models/Node.cs
+++ Hitori/Models/Node.cs
@@ -3,23 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace Hitori.Models
 {
     class Node
     {
-        private int xpos,ypos;
-        private List<Node> AdjaList;
+        private Button _button;
+        private State _state;
+        private int _value;
+        private int _xpos,_ypos;
+        private List<Node> adjaList;
 
-        public Node(int x, int y,  List<Node> list)
+        public Node(int x, int y, int value, State state)
         {
-            xpos = x;
-            ypos = y;
-            for (int i = 0; i <= list.Count; i++)
-            {
-                AdjaList.Add(list[i]);
-            }
+            this._xpos = x;
+            this._ypos = y;
+            this._value = value;
+            this._state = state;
         }
-
+        public Button Button { get => _button; set => _button = value; }
+        public int Value { get => _value; set => _value = value; }
+        internal State State { get => _state; set => _state = value; }
+        public int Xpos { get => _xpos; set => _xpos = value; }
+        public int Ypos { get => _ypos; set => _ypos = value; }
+        public List<Node> AdjaList { get => adjaList; set => AdjaList = value; }
     }
 }
