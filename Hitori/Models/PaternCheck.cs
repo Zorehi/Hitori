2ef78e38b926e04429f1bff6f5cf52049f094539
CyrilJack <jacques.cyril80@gmail.com>
2023-11-17 11:43:45 +0100

    Ajout du pattern croix

--- Hitori/Models/PaternCheck.cs
+++ Hitori/Models/PaternCheck.cs
@@ -13,11 +13,12 @@ namespace Hitori.Models
         {
             int len = hitori.Nodes.GetLength(0);
             int valueA, valueB, valueC, valueD;
-
+            // Cas où le somment est un coin : on ne fait rien 
             if ((node.Xpos == 0 && node.Ypos == 0) || (node.Xpos == len - 1 && node.Ypos == 0) || (node.Xpos == len - 1 && node.Ypos == len - 1) || (node.Xpos == len - 1 && node.Ypos == len - 1))
             {
 
             }
+            // Cas où le sommet est en haut ou en bas de la grille 
             else if (node.Xpos == 0 || node.Xpos == len - 1)
             {
                 valueA = hitori.Nodes[node.Xpos, node.Ypos - 1].Box.Value;
@@ -27,6 +28,7 @@ namespace Hitori.Models
                     node.Box.IsLock = true;
                 }
             }
+            // Cas où le sommet est à gauche ou à droite de la grille
             else if (node.Ypos == 0 || node.Ypos == len - 1)
             {
                 valueA = hitori.Nodes[node.Xpos - 1, node.Ypos].Box.Value;
@@ -49,7 +51,9 @@ namespace Hitori.Models
 
             }
         }
-
+        // Doublet = Si deux cases adja ont la même valeur, alors tous les cases dans la même ligne ou colonne ayant la même valeur sont blanche
+        // Triplet = Si trois cases adja ont la même valeur, alors celle du milieu est forcément noire et les deux extrémités sont blancs,
+        // et toutes les cases dans la même ligne ou colonne ayant la même valeur que le triplet et blanche
         static public void Doublet(Hitori hitori, Node node)
         {
             int len = hitori.Nodes.GetLength(0);
@@ -58,7 +62,7 @@ namespace Hitori.Models
             int cntY = 0;
             List<int> lxPos = new List<int>();
             List<int> lyPos = new List<int>();
-
+            // On vérifie si les voisins su sommets ont la même valeur que lui 
             for (int i = 0; i<count; i++)
             {
                 if(node.AdjaList[i].Box.Value == node.Box.Value)
@@ -75,6 +79,7 @@ namespace Hitori.Models
                     }
                 }
             }
+            // S'il n'y a qu'un doublet sur sa ligne 
             if (cntX == 1)
             {
                 for (int j = 0; j < len; j++)
@@ -89,6 +94,7 @@ namespace Hitori.Models
                     }
                 }
             }
+            // Si c'est un triplet sur sa ligne  
             else if (cntX == 2)
             {
                 node.Box.IsLock = true;
@@ -104,6 +110,7 @@ namespace Hitori.Models
                     }
                 }
             }
+            // S'il n'y a qu'un doublet sur sa colonne 
             else if (cntY == 1)
             {
                 for (int j = 0; j < len; j++)
@@ -119,6 +126,7 @@ namespace Hitori.Models
 
                 }
             }
+            // Si c'est un triplet sur sa colonne 
             else if (cntY == 2)
             {
                 node.Box.IsLock = true;
@@ -134,6 +142,54 @@ namespace Hitori.Models
                     }
                 }
 
+            }
+        }
+        static public void Croix(Hitori hitori, Node node)
+        {
+            int len = hitori.Nodes.GetLength(0); 
+            for (int i = 0; i<node.AdjaList.Count; i++)
+            {
+                if(node.Xpos == node.AdjaList[i].Xpos) // Voisin sur la même ligne 
+                {
+                    for (int j = 1; j< len-1; j++)
+                    {
+                        if (hitori.Nodes[j,node.Ypos].Box.Value == node.Box.Value) // S'il existe une autre case avec la même valeur que la case initial 
+                        {
+                            if (node.AdjaList[i].Box.Value == hitori.Nodes[j-1, node.AdjaList[i].Ypos].Box.Value)
+                            {
+                                hitori.Nodes[j - 1, node.Ypos].Box.IsLock = true;
+                                hitori.Nodes[j, node.AdjaList[i].Ypos].Box.IsLock = true;
+
+                            }
+                            else if (node.AdjaList[i].Box.Value == hitori.Nodes[j + 1, node.AdjaList[i].Ypos].Box.Value)
+                            {
+                                hitori.Nodes[j + 1, node.Ypos].Box.IsLock = true;
+                                hitori.Nodes[j, node.AdjaList[i].Ypos].Box.IsLock = true;
+                            }
+
+
+                        }
+                    }
+                }
+                else if (node.Ypos == node.AdjaList[i].Ypos)
+                {
+                    for (int k = 1; k < len - 1; k++)
+                    {
+                        if (hitori.Nodes[node.Xpos, k].Box.Value == node.Box.Value)
+                        {
+                            if (node.AdjaList[i].Box.Value == hitori.Nodes[node.AdjaList[i].Xpos, k-1].Box.Value)
+                            {
+                                hitori.Nodes[node.Xpos,k-1].Box.IsLock = true;
+                                hitori.Nodes[node.AdjaList[i].Xpos, k].Box.IsLock = true;
+
+                            }
+                            else if (node.AdjaList[i].Box.Value == hitori.Nodes[j + 1, node.AdjaList[i].Ypos].Box.Value)
+                            {
+                                hitori.Nodes[j + 1, node.Ypos].Box.IsLock = true;
+                                hitori.Nodes[j, node.AdjaList[i].Ypos].Box.IsLock = true;
+                            }
+                        }
+                }
             }
         }
     }
